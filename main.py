06faf49aff99a42e31c5564635a9bca4d5991e53
Taufik <taufik4051@gmail.com>
2025-10-22 16:42:23 +0530

    new pr

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import numpy as np
+from win11toast import toast
+from datetime import datetime
+import time
 
-n = np.array([1, 2, 3])
-m = np.array([4, 5, 6])
+while True:
+    f = toast('Hi', 'Did you drink water?', buttons= ["Yes", "No"])
 
-print(n * m)
+    if f.get("arguments") == 'http:Yes':
+        now = datetime.now()
+        current_date = now.date()
+        current_time = now.time()
+        print(f"yes at {current_date} {current_time}")
+        toast('😊', 'Keep it up!')
+
+    elif f.get("arguments") == 'http:No':
+        now = datetime.now()
+        current_date = now.date()
+        current_time = now.time()
+        print(f"no at {current_date} {current_time}")
+        toast('😤', 'Do it next time!!')
+    
+    time.sleep(1800)
